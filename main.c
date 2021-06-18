dc66f7274299a8de643f9b7349361606fdab5097
Iliana Karakasheva <85765067+IlianaKarakasheva@users.noreply.github.com>
2021-06-18 22:24:45 +0300

    zapisvame vuv fail!
    git push oshte ne raboti i zatova taka!!

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ struct board{
     char filename[100];
     int width;
     int height;
+    int turn_side;
 };
 
 void init_board(struct board *board){
@@ -23,59 +24,8 @@ void init_board(struct board *board){
         scanf("%d", &board->width);
     }while(board->width < 4 || board->width > 40);
 
+    board->turn_side = 0;
 } 
-
-void draw_board(struct board *board, int turn, int invalid){
-    char symb;
-    for (int i = 0; i < board->height; i++)
-    {
-        printf("\n");
-        for (int j = 0; j < board->width; j++)
-        {     
-            if(board->placeholders[i][j] == 0) symb = ' ';
-            else{
-                symb = board->placeholders[i][j];
-            }
-            printf("|%c", symb);
-        }   
-        printf("|");
-
-        if(i == 0) {
-            if (turn == 0){
-                printf("        Player 1");
-            }
-            if (turn == 1){
-                printf("        Player 2");
-            }
-        }
-        if(i == 3){
-            if (invalid == 1){
-                printf("        !Invalid input!");
-            }
-        }
-    }
-    printf("\n");
-    for (int i = 0; i < board->width; i++)
-    {
-        printf(" %d", i+1);
-    }
-    printf("\n\n");
-}
-
-void make_move(struct board *main_board, int pos, int turn){
-
-        for (int i = main_board->height-1; i >= 0; i--)
-        {
-            if(main_board->placeholders[i][pos] == 0){
-
-                if(turn == 0)
-                    main_board->placeholders[i][pos] = 'O';
-                else 
-                    main_board->placeholders[i][pos] = 'X';
-                break;
-            }
-        }
-}
 
 int check_for_win(struct board *board){
     for (int i = 0; i < board->height; i++)
@@ -122,39 +72,119 @@ int check_for_win(struct board *board){
     return 0;
 }
 
+void draw_board(struct board *board, int invalid){
+    char symb;
+    for (int i = 0; i < board->height; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < board->width; j++)
+        {     
+            if(board->placeholders[i][j] == 0) symb = ' ';
+            else{
+                symb = board->placeholders[i][j];
+            }
+            printf("|%c", symb);
+        }   
+        printf("|");
+
+        if(i == 0) {
+            if (board->turn_side == 0){
+                printf("        Player 1");
+            }
+            if (board->turn_side == 1){
+                printf("        Player 2");
+            }
+        }
+        if(i == 3){
+            if (invalid == 1){
+                printf("        !Invalid input!");
+            }
+        }
+    }
+    printf("\n");
+    for (int i = 0; i < board->width; i++)
+    {
+        printf(" %d", i+1);
+    }
+    printf("\n\n");
+
+    int found_winner = check_for_win(board);
+    if(found_winner){
+        if(found_winner == 1) printf("Player 1 wins\n");
+        if(found_winner == 2) printf("Player 2 wins\n");
+    }
+}
+
+void make_move(struct board *main_board, int pos){
+
+        for (int i = main_board->height-1; i >= 0; i--)
+        {
+            if(main_board->placeholders[i][pos] == 0){
+
+                if(main_board->turn_side == 0)
+                    main_board->placeholders[i][pos] = 'O';
+                else 
+                    main_board->placeholders[i][pos] = 'X';
+                break;
+            }
+        }
+}
+
+void save_in_file(struct board *board){
+    FILE * fp;
+    char symb;
+    fp = fopen (board->filename,"a");
+
+    for (int i = 0; i < board->height; i++)
+    {
+        fprintf(fp, "\n");
+        for (int j = 0; j < board->width; j++)
+        {     
+            if(board->placeholders[i][j] == 0) symb = ' ';
+            else{
+                symb = board->placeholders[i][j];
+            }
+            fprintf(fp, "|%c", symb);
+        }   
+        fprintf(fp, "|");
+    }
+    fprintf(fp, "\n");
+    fclose (fp);
+}
+
 int main(){
     struct board main_board = {0};
 
     init_board(&main_board);
-    draw_board(&main_board, 0, 0);
+    draw_board(&main_board, 0);
 
-    int in_game = 0, turn = 0;
+    int in_game = 0;
     while (!in_game)
     {
-        //printf("\nPLAYER %d\n", turn + 1);
 
         int pos = 0;
+        // Read player's move
         do
         {
             printf("Type a number between 1-%d: ", main_board.width);
             scanf("%d", &pos);
             if (pos < 1 || pos > main_board.width || main_board.placeholders[0][pos-1] != 0)
             {
-                draw_board(&main_board, turn, 1);
+                draw_board(&main_board, 1);
             }
 
-        } while (pos < 1 || pos > main_board.width || main_board.placeholders[0][pos-1] != 0);        
+        } while (pos < 1 || pos > main_board.width || main_board.placeholders[0][pos-1] != 0); 
+        // Adjust to 0 base       
         pos -= 1;
 
-        make_move(&main_board, pos, turn);
+        make_move(&main_board, pos);
 
         in_game = check_for_win(&main_board);
         
-        if(turn == 0) turn = 1;
-        else turn = 0;
+        // Switch sides
+        main_board.turn_side = !main_board.turn_side; 
 
-        draw_board(&main_board, turn, 0);
+        save_in_file(&main_board);
+        draw_board(&main_board, 0);
     }
-    if(in_game == 1) printf("Player 1 wins\n");
-    if(in_game == 2) printf("Player 2 wins\n");
 }
